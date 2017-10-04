410d5d010d0f92f317a559a80cd8e4cd84e58be7
AROSTIROLLA <anderson.rostirolla@videplast.com.br>
2017-10-04 13:07:52 -0300

    Metodo post para testar

--- ClientREST/Form1.Designer.cs
+++ ClientREST/Form1.Designer.cs
@@ -33,57 +33,186 @@
             this.tbResposta = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbNumViz = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbMin = new System.Windows.Forms.TextBox();
+            this.tbMax = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbIdSensor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ckTrataTempoReal = new System.Windows.Forms.CheckBox();
+            this.ckPopularBase = new System.Windows.Forms.CheckBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btConectar
             // 
-            this.btConectar.Location = new System.Drawing.Point(387, 12);
+            this.btConectar.Location = new System.Drawing.Point(115, 192);
             this.btConectar.Name = "btConectar";
             this.btConectar.Size = new System.Drawing.Size(75, 23);
-            this.btConectar.TabIndex = 0;
-            this.btConectar.Text = "Conectar";
+            this.btConectar.TabIndex = 9;
+            this.btConectar.Text = "Ir";
             this.btConectar.UseVisualStyleBackColor = true;
             this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
             // 
             // tbRequest
             // 
-            this.tbRequest.Location = new System.Drawing.Point(127, 12);
+            this.tbRequest.Location = new System.Drawing.Point(115, 12);
             this.tbRequest.Name = "tbRequest";
             this.tbRequest.Size = new System.Drawing.Size(254, 20);
             this.tbRequest.TabIndex = 1;
             // 
             // tbResposta
             // 
-            this.tbResposta.Location = new System.Drawing.Point(127, 38);
+            this.tbResposta.Location = new System.Drawing.Point(115, 224);
             this.tbResposta.Multiline = true;
             this.tbResposta.Name = "tbResposta";
             this.tbResposta.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbResposta.Size = new System.Drawing.Size(401, 181);
-            this.tbResposta.TabIndex = 2;
+            this.tbResposta.Size = new System.Drawing.Size(401, 115);
+            this.tbResposta.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 17);
+            this.label1.Location = new System.Drawing.Point(42, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Request URL:";
+            this.label1.Text = "URL da API:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 41);
+            this.label2.Location = new System.Drawing.Point(19, 227);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Response URL:";
+            this.label2.Text = "Resposta da API:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Numero de vizinhos:";
+            // 
+            // cbNumViz
+            // 
+            this.cbNumViz.FormattingEnabled = true;
+            this.cbNumViz.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cbNumViz.Location = new System.Drawing.Point(115, 90);
+            this.cbNumViz.Name = "cbNumViz";
+            this.cbNumViz.Size = new System.Drawing.Size(39, 21);
+            this.cbNumViz.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor minimo:";
+            // 
+            // tbMin
+            // 
+            this.tbMin.Location = new System.Drawing.Point(115, 117);
+            this.tbMin.Name = "tbMin";
+            this.tbMin.Size = new System.Drawing.Size(56, 20);
+            this.tbMin.TabIndex = 5;
+            // 
+            // tbMax
+            // 
+            this.tbMax.Location = new System.Drawing.Point(115, 143);
+            this.tbMax.Name = "tbMax";
+            this.tbMax.Size = new System.Drawing.Size(56, 20);
+            this.tbMax.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Valor maximo:";
+            // 
+            // tbIdSensor
+            // 
+            this.tbIdSensor.Location = new System.Drawing.Point(115, 38);
+            this.tbIdSensor.Name = "tbIdSensor";
+            this.tbIdSensor.Size = new System.Drawing.Size(56, 20);
+            this.tbIdSensor.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Id do sensor:";
+            // 
+            // ckTrataTempoReal
+            // 
+            this.ckTrataTempoReal.AutoSize = true;
+            this.ckTrataTempoReal.Location = new System.Drawing.Point(115, 169);
+            this.ckTrataTempoReal.Name = "ckTrataTempoReal";
+            this.ckTrataTempoReal.Size = new System.Drawing.Size(149, 17);
+            this.ckTrataTempoReal.TabIndex = 7;
+            this.ckTrataTempoReal.Text = "Tratamento em tempo real";
+            this.ckTrataTempoReal.UseVisualStyleBackColor = true;
+            // 
+            // ckPopularBase
+            // 
+            this.ckPopularBase.AutoSize = true;
+            this.ckPopularBase.Location = new System.Drawing.Point(270, 169);
+            this.ckPopularBase.Name = "ckPopularBase";
+            this.ckPopularBase.Size = new System.Drawing.Size(114, 17);
+            this.ckPopularBase.TabIndex = 8;
+            this.ckPopularBase.Text = "Popular base teste";
+            this.ckPopularBase.UseVisualStyleBackColor = true;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(115, 64);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(56, 20);
+            this.tbValor.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(75, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Valor:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 231);
+            this.ClientSize = new System.Drawing.Size(520, 351);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ckPopularBase);
+            this.Controls.Add(this.ckTrataTempoReal);
+            this.Controls.Add(this.tbIdSensor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbMax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbMin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbNumViz);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbResposta);
@@ -103,6 +232,18 @@
         private System.Windows.Forms.TextBox tbResposta;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbNumViz;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbMin;
+        private System.Windows.Forms.TextBox tbMax;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbIdSensor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox ckTrataTempoReal;
+        private System.Windows.Forms.CheckBox ckPopularBase;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- ClientREST/Form1.cs
+++ ClientREST/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace ClientREST
 {
@@ -20,14 +22,60 @@ namespace ClientREST
         #region UI Event Handlers
         private void btConectar_Click(object sender, EventArgs e)
         {
-            RestClient rClient = new RestClient();
+            //metodo get
+            /*RestClient rClient = new RestClient();
             rClient.endPoint = tbRequest.Text;
             string strResponse = string.Empty;
             strResponse = rClient.makeRequest();
 
-            debugOutput(strResponse);
+            debugOutput(strResponse);*/
+
+            //metodo post
+            SubmitData();
         }
         #endregion
+
+        private void SubmitData()
+        {
+            try
+            {
+                int idSensor = Convert.ToInt32(tbIdSensor.Text);
+                double valor = Convert.ToDouble(tbValor.Text);
+                double max = Convert.ToDouble(tbMax.Text);
+                double min = Convert.ToDouble(tbMin.Text);
+                int vizinhos = Convert.ToInt32(cbNumViz.SelectedItem.ToString());
+                bool tratamentoTempoReal = (ckTrataTempoReal.Checked) ? true : false;
+                bool popularBaseTeste = (ckPopularBase.Checked) ? true : false;
+
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                string postData = "?idSensor=" + idSensor + "&valor=" + valor + "&min=" + min + "&max=" + max + "&redeSensor=" + tratamentoTempoReal + "&vizinhos=" + vizinhos;
+
+                byte[] data = encoding.GetBytes(postData);
+
+                WebRequest request = WebRequest.Create(tbRequest.Text);
+                request.Method = "POST";
+                request.ContentType = "application/form-data";
+                request.ContentLength = data.Length;
+
+                Stream stream = request.GetRequestStream();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+
+                WebResponse response = request.GetResponse();
+                stream = response.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+
+                MessageBox.Show(sr.ReadToEnd() + "Deu boa");
+
+                sr.Close();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
 
         private void debugOutput(string strDebugText)
         {
